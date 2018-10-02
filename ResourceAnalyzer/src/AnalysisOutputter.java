ed29d35003e10e658b5f12267293d503c517291b
genek96 <genek_96@mail.ru>
2018-10-02 15:53:26 +0300

    Delete AnalysisOutputter.java

--- ResourceAnalyzer/src/AnalysisOutputter.java
+++
@@ -1,7 +0,0 @@
-import java.io.IOException;
-import java.io.OutputStream;
-import java.util.HashMap;
-
-public interface AnalysisOutputter {
-    void outputAnalysis(HashMap<String, Integer> analysis, OutputStream outputStream) throws IOException;
-}
